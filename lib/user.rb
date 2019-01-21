8c39c95515d7f507e21acb2561183365412445eb
RajaomalalaSendra <rajaomalalasendrampanjato@gmail.com>
2019-01-21 13:26:21 +0300

    updating

--- lib/user.rb
+++ lib/user.rb
@@ -2,16 +2,26 @@ class User
 	attr_accessor :email, :name
 	@@counter = 0
 	# creation of the email
-	def initializer(email)
-		
+	def initialize(myemail)
+		@email = myemail
+		puts myemail
 	end
 	# creation of the name
-	def name(name)
-		name = @@myname
-		return @@myname
+	def name(myname)
+		@name = myname
+		puts myname
 	end
 	# creation of the counter
 	def self.count
-		@@counter
+		@@counter += 1
+		puts @@counter
 	end
 end
+user = User.new("man@man.com")
+user.email
+user.name("Rakoto")
+User.count
+user2 = User.new("men@men.com")
+user2.email
+user2.name("Rajao")
+User.count
